54bf86cd65b34e8dea7a94c678e23e159d6344df
DaanVanYperen <daan@mostlyoriginal.net>
2019-04-28 23:24:39 +0200

    Add smoke effect when placing a spawner.

--- core/src/net/mostlyoriginal/game/system/control/DeploySystem.java
+++ core/src/net/mostlyoriginal/game/system/control/DeploySystem.java
@@ -6,6 +6,7 @@ import net.mostlyoriginal.game.GameRules;
 import net.mostlyoriginal.game.component.GridPos;
 import net.mostlyoriginal.game.component.ItemData;
 import net.mostlyoriginal.game.manager.ItemRepository;
+import net.mostlyoriginal.game.system.ParticleSystem;
 import net.mostlyoriginal.game.system.SlotHighlightingSystem;
 
 /**
@@ -15,6 +16,7 @@ public class DeploySystem extends BaseSystem {
 
     ItemRepository itemRepository;
     SlotHighlightingSystem slotHighlightingSystem;
+    private ParticleSystem particleSystem;
 
     @Override
     protected void processSystem() {
@@ -40,10 +42,17 @@ public class DeploySystem extends BaseSystem {
         // add spawners on each cell.
         for (int y = 0; y < 2; y++) {
             for (int x = 0; x < 2; x++) {
+                int gx = (originGridX - slotOffsetX) + x;
+                int gy = (originGridY - slotOffsetY) + y;
                 E.E().gridPos(
-                        (originGridX - slotOffsetX) + x,
-                        (originGridY - slotOffsetY) + y)
+                        gx,
+                        gy)
                         .passiveSpawnerItems(itemData.machineProducts);
+
+                particleSystem.poof(
+                        gx * GameRules.CELL_SIZE + 16,
+                        gy * GameRules.CELL_SIZE + 16, 40, 40, ParticleSystem.COLOR_WHITE_TRANSPARENT);
+
             }
         }
 
