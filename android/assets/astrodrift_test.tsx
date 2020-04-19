43feeeaa3427d47d49bdbaed4907c661a1d11bde
Flaterectomy <michielbarten@gmail.com>
2020-04-19 12:53:27 +0200

    Added custom properties to tiles

---
+++ android/assets/astrodrift_test.tsx
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Astrodrift" tilewidth="48" tileheight="48" tilecount="441" columns="21">
+ <image source="C:/github/ld46/odb-moistfling/android/assets/tileset.png" width="1024" height="1024"/>
+ <terraintypes>
+  <terrain name="New Terrain" tile="106"/>
+ </terraintypes>
+ <tile id="84">
+  <properties>
+   <property name="anim" value="debris_small_1"/>
+   <property name="entity" value="debris"/>
+   <property name="invisible" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name="anim" value="debris_small_2"/>
+   <property name="entity" value="debris"/>
+   <property name="invisible" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="86">
+  <properties>
+   <property name="anim" value="debris_small_3"/>
+   <property name="entity" value="debris"/>
+   <property name="invisible" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="87">
+  <properties>
+   <property name="anim" value="debris_big_1"/>
+   <property name="entity" value="debris"/>
+   <property name="invisible" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="89">
+  <properties>
+   <property name="anim" value="debris_medium_1"/>
+   <property name="entity" value="debris"/>
+   <property name="invisible" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <properties>
+   <property name="anim" value="debris_medium_2"/>
+   <property name="entity" value="debris"/>
+   <property name="invisible" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="105" terrain=",0,,">
+  <properties>
+   <property name="anim" value="debris_small_4"/>
+   <property name="entity" value="debris"/>
+   <property name="invisible" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="106" terrain="0,0,,0">
+  <properties>
+   <property name="anim" value="debris_small_5"/>
+   <property name="entity" value="debris"/>
+   <property name="invisible" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="107">
+  <properties>
+   <property name="anim" value="debris_small_6"/>
+   <property name="entity" value="debris"/>
+   <property name="invisible" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
